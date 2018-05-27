94670c6a8c6dc72ede64db16d338e31c3d567d25
david <david11sone@gmail.com>
2018-05-27 16:33:05 +0530

    ssssss

--- Prototypes/string.paragraph.prop.js
+++ Prototypes/string.paragraph.prop.js
@@ -14,13 +14,18 @@
     var readFile = function (path, fname) {
         try {
             var s = "";
+            var paraCount= 0;
             fs.readFile(path + fname, function (err, data) {
                 if (err) throw new Error("Error in Reading data :" + err);
                  s += data.toString('utf8');
                  var rows = s.split("\n");
                  for (var row_num  = 0; row_num  < rows.length - 1; row_num++){
-                    console.log(lines[row_num]);
+                    console.log(rows[row_num]);
+                     if(rows[row_num].indexOf(".")!==-1){
+                        paraCount++;
+                     }
                  }
+                 console.log("No of Para :"+paraCount);
             });
         } catch (exp) {
             throw new Error("Error in Reading File :" + e);
